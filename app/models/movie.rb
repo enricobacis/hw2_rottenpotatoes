9b8a04dc048824146329379974cb8ebd747de77f
Enrico Bacis <enrico.bacis@gmail.com>
2012-07-31 17:45:48 +0200

    Removed method_missing (check later), implemented simple all_ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,16 @@
 class Movie < ActiveRecord::Base
-  def method_missing(method_id)
-    /^all_(\w*)s$/.match(method_id.to_s)
-    if self.column_names.include? $1
-      self.select($1).group($1).map{|t| t.send($1)}
-    else
-      super
-    end
+
+  def all_ratings
+    self.select(:rating).group(:rating).map{|t| t.rating}
   end
+
+#  def method_missing(method_id)
+#    /^all_(\w*)s$/.match(method_id.to_s)
+#    if self.column_names.include? $1
+#      self.select($1).group($1).map{|t| t.send($1)}
+#    else
+#      super
+#    end
+#  end
+
 end
