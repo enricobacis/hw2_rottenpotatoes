fd6e4392e475fa3f994701d899461fefc09d61b6
Enrico Bacis <enrico.bacis@gmail.com>
2012-07-31 18:01:51 +0200

    typo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,15 @@
 class Movie < ActiveRecord::Base
 
-  def all_ratings
-    self.select(:rating).group(:rating).map{|t| t.rating}
+  def self.all_ratings
+    select(:rating).group(:rating).map{|t| t.rating}
   end
 
-#  def method_missing(method_id)
-#    /^all_(\w*)s$/.match(method_id.to_s)
+#  def self.method_missing(method, *args, &block)
+#    /^all_(\w*)s$/.match(method.to_s)
 #    if self.column_names.include? $1
 #      self.select($1).group($1).map{|t| t.send($1)}
 #    else
-#      super
+#      super(method, *args, &block)
 #    end
 #  end
 
